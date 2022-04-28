72133bcc18806966559f15ef4b3c1ab68a669c8b
ungerecht <kevinungerecht@gmail.com>
2022-04-27 17:17:13 -0700

    Move sort to fetching of conversations

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -8,10 +8,7 @@ const Messages = (props) => {
 
   return (
     <Box>
-      {messages.map((element, index, messages) => {
-        //get message from end of array
-        const message = messages[messages.length - 1 - index];
-
+      {messages.map((message) => {
         const time = moment(message.createdAt).format('h:mm');
 
         return message.senderId === userId ? (
